f730fe3bfb4f7f59426e74dad37ed54ea855b221
Gautier couture <2017566295@lacatholille.fr>
2020-11-10 11:21:58 +0100

    carrousel get api data

--- carrousel/src/App.vue
+++ carrousel/src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div id='app'>
-    <b-carousel ref="myCarousel" id="carousel-1" v-model="slide" :interval="4000" controls indicators background="#ababab" img-width="220" img-height="100" style="text-shadow: 1px 1px 2px #333;" @sliding-start="onSlideStart" @sliding-end="onSlideEnd">
-      <b-carousel-slide img-src="http://62.210.247.201:9000/BTWIN.png"></b-carousel-slide>
-      <b-carousel-slide img-src="http://62.210.247.201:9000/adidas.png"></b-carousel-slide>
-      <b-carousel-slide img-src="http://62.210.247.201:9000/bhfitness.png"></b-carousel-slide>
-      <b-carousel-slide img-src="http://62.210.247.201:9000/domyos.png"></b-carousel-slide>
+    <div class="d-flex justify-content-center">
+      <img src="./assets/logo.png">
+    </div>
+    <b-carousel class="justify-content-center w-25 p-3 d-flex" ref="myCarousel" id="carousel-1" v-model="slide" :interval="4000" controls indicators background="#fff" img-width="1024" img-height="480" style="text-shadow: 1px 1px 2px #333;">
+      <div  v-for="image in images" :key="image.image">
+        <b-carousel-slide v-bind:img-src="image.image"></b-carousel-slide>
+      </div>
     </b-carousel>
+     <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="http://62.210.247.201:9000/bhfitness.png"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="http://62.210.247.201:9000/bhfitness.png">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+    <p class="d-flex justify-content-center">Par Gautier Couture</p>
   </div>
 </template>
 
@@ -16,8 +29,9 @@ export default {
   name: 'App',
   components: {
   },
-  data() {
+  data: function() {
       return {
+         images: [],
         slide: 0,
         sliding: null
       }
@@ -27,7 +41,7 @@ export default {
   mounted () {
     axios
       .get('http://62.210.247.201:9000/test')
-      .then(response => (this.images = response))
+      .then(response => (this.images = response.data))
       .catch(error => {
         console.log(error)
         this.errored = true})
@@ -35,8 +49,11 @@ export default {
 }
 </script>
 <style>
-  #app{
-    width: 50%;
-    display: center;
-  }
+#app{
+  background-color: rgb(241, 214, 0);
+}
+
+b-carousel-slide:hover{
+  background-color: rgb(241, 214, 0);
+}
 </style>
